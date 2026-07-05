5b1dc48af907c847d495865a9f96f2e77e9a2f1e
Risha Dinesh <dinesh.ri@northeastern.edu>
2026-07-05 14:18:56 -0400

    added moving average crossover strategy

--- 01-stock-analysis/main.py
+++ 01-stock-analysis/main.py
@@ -15,7 +15,16 @@ returns = close_prices.pct_change() # pct_change() calculates day-over-day retur
 print(returns.head(10) * 100) #displayed as a percentage
 
 #Calculate cumulative returns
-cumulative_returns = (1+ returns).cumprod()
+cumulative_returns = (1 + returns).cumprod()
+
+# calculate the daily volatility (standard deviation) for each stock over 30-day rolling window
+rolling_vol = returns.rolling(window = 30).std()
+
+
+# Trading Strategy: Long and Short Run Moving Average Crossover: for ICLN
+short_mavg = close_prices['ICLN'].rolling(window = 20).mean()
+long_mavg = close_prices['ICLN'].rolling(window = 50).mean()
+
 
 # daily returns chart
 returns.plot(title="Daily Returns: ICLN vs SPY (2020–Present)")
@@ -31,6 +40,37 @@ plt.ylabel("Growth of $1 Invested")
 plt.axhline(y=1, color='gray', linestyle='--', linewidth=0.8)
 plt.show()
 
+# 30- day rolling volatility chart
+
+rolling_vol.plot(title="30-Day Rolling Volatility: ICLN vs SPY (2020–Present)")
+plt.xlabel("Date")
+plt.ylabel("Volatility")
+plt.axhline(y=0, color='gray', linestyle='--', linewidth=0.8)
+plt.show()
+
+# moving average crossover chart with buy and sell signals on chart
+buy_sig = (short_mavg > long_mavg) & (short_mavg.shift(1) <= long_mavg.shift(1))
+sell_sig = (short_mavg < long_mavg) & (short_mavg.shift(1) >= long_mavg.shift(1))
+
+buy_dates = buy_sig[buy_sig].index
+sell_dates = sell_sig[sell_sig].index
+
+close_prices['ICLN'].plot(label = "ICLN Prices")
+long_mavg.plot(label = "50 Day Moving Average")
+short_mavg.plot(label = "20 Day Moving Average")
+
+plt.scatter(buy_dates, close_prices['ICLN'][buy_dates], marker = '^', color = 'green', s = 100,  label = 'Buy', zorder = 5)
+plt.scatter(sell_dates, close_prices['ICLN'][sell_dates], marker = 'v', color = 'red', s = 100, label = 'Sell', zorder = 5)
+
+
+plt.title("ICLN Price and Moving Average Crossovers (2020–Present)")
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.legend()
+plt.show()
+
+
+
 # calculate variance and standard deviation for each ticker
 variance = returns.var()
 std_dev = returns.std()
@@ -43,3 +83,4 @@ print(std_dev)
 avg_return = returns.mean()
 print("\nAverage Daily Return:")
 print(avg_return)
+
